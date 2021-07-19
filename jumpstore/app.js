d75a1b79b3bcfe55e958628f6f7872f7d3d01913
martin-marzer <martinvazquez1104@gmail.com>
2021-07-19 02:31:48 -0300

    agregue header y footer

---
+++ jumpstore/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+
+
+app.listen(3030, () => {
+    console.log("Levantando un servidor con Express");
+})
