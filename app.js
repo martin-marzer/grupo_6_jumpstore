2458231457f2cfd6e6cac9d21c3936129a1503ce
martin-marzer <martinvazquez1104@gmail.com>
2021-07-22 07:07:21 -0300

    terminado productos y fotos ordenadas

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-
 app.use(express.static("public"));
+
+app.listen(3030, () => {
+    console.log("Levantando un servidor con Express");
+})
+
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
@@ -16,8 +21,8 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 })
 
-
-
-app.listen(3030, () => {
-    console.log("Levantando un servidor con Express");
+app.get("/product/detail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/detailProduct.html"))
 })
+
+
