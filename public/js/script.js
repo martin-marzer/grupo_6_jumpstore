f68a3c54e36dba3f5cff49f677ef471b4a453e46
martin-marzer <martinvazquez1104@gmail.com>
2021-07-23 00:40:09 -0300

    agregue animaciones

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,29 @@
-function myFunction() {
-    let x = document.getElementById("jumpstore-navbar");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("jumpstore-navbar-mobile").style.width = "80%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("jumpstore-navbar-mobile").style.width = "0";
+}
+
+
+var acc = document.getElementsByClassName("jumpstore-arrow");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
     } else {
-      x.style.display = "none";
+      panel.style.display = "block";
     }
-  }
+  });
+}
