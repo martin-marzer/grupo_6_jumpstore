e5f9851ac64330acf255e140e00156296bd59f43
martin-marzer <martinvazquez1104@gmail.com>
2021-07-25 19:43:16 -0300

    como se ve en internet

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3030;
 
 app.use(express.static("public"));
 
-app.listen(3030, () => {
+app.listen(PORT, () => {
     console.log("Levantando un servidor con Express");
 })
 
