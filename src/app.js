e824232ceba996193262854610874da658658c40
martin-marzer <martinvazquez1104@gmail.com>
2021-08-13 15:19:48 -0300

    agregue lectura json

--- src/app.js
+++ src/app.js
@@ -6,14 +6,21 @@ const rutaProducts = require("./routes/products");
 const rutaUsers = require("./routes/users");
 
 app.use(express.static("public"));
+app.use(express.json());
+
+
 app.set("view engine", "ejs");
 app.set("views", "./src/views")
+
+
+
+app.use(rutaMain);
+app.use(rutaProducts);
+app.use(rutaUsers);
+
+
 
 app.listen(PORT, () => {
     console.log("Todo correcto");
 
 })
-
-app.use(rutaMain);
-app.use(rutaProducts);
-app.use(rutaUsers);
