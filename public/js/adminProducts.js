648201d7a81cb58d1d0580b4f2b04b8932edad9b
martin-marzer <martinvazquez1104@gmail.com>
2021-08-29 15:40:09 -0300

    cambio admin css y js en proceso

---
+++ public/js/adminProducts.js
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let productsContainer = document.querySelectorAll(".products-container")[0].children
+// // console.log(productsContainer)
+// let products = document.querySelectorAll(".product-container")
+// let productsNew = []
+// productsContainer.innerHTML += productsNew.innerHTML; 
+// // let info = document.querySelectorAll(".titulos_conteiner li");
+// // [0].innerHTML
+// // let eachINFO
+// // , eachINFOtext = [];
+// // for (let i = 0; i < 4; i++) {
+// //     eachINFO = info[i];
+// //     eachINFOtext.push(eachINFO.innerHTML)
+// //     console.log(eachINFO)
+// // }
+// // eachINFO.onclick = hola;
+// // function hola() {
+// //     alert(eachINFOtext)
+// // }
+// // console.log(info)
+// let info = document.querySelectorAll(".titulos_conteiner li")[1];
+// info.onclick = sortName;
+// let order = false;
+// function sortName(){
+//     // order = !order;
+//     // products.sort(function (a, b) {
+//     //   const x = a.name.toLowerCase();
+//     //   const y = b.name.toLowerCase();
+//     //   return (order ? x > y : x < y);
+//     // });
+//     // console.log(products);
+//     // let reverse = products.reverse
+//     // products = reverse
+//     for (let i = 0; i < products.length; i++) {
+//         let eachproduct = products[i];
+//         productsNew.unshift(eachproduct)
+        
+        
+        
+//     }
+//     console.log(productsNew)
+//     alert("hola")
+//   }
+// console.log(info.innerHTML)
+// //   let finalProduct = JSON.stringify(newBasedata, null, 2)
+
+// //   fs.writeFileSync(productsFilePath, finalProduct) 
