731282660dc9d50451c896807df8789f89a79155
martin-marzer <martinvazquez1104@gmail.com>
2021-09-05 01:58:55 -0300

    home deruta

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,14 @@
-
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.splide', {
         type   : 'loop',
         perPage: 3,
         perMove: 1,
         width: 1000,
-        pagination: "false"
+        pagination: "false",
+        classes: {
+            arrow : 'splide__arrow arrow-splide',
+            prev  : 'splide__arrow--prev arrow-splide-prev',
+            next  : 'splide__arrow--next arrow-splide-next',
+        },
 	} ).mount();
 } );
