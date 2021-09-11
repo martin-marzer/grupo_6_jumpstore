810d7ae8fd07b88ecab0e74ac800e51f39883910
martin-marzer <martinvazquez1104@gmail.com>
2021-09-11 04:00:28 -0300

    register logic has been done

--- public/js/register-login.js
+++ public/js/register-login.js
@@ -1,32 +1,25 @@
 let form = document.getElementById("password")
 console.log(form)
-let eyePsw = document.querySelector(".field.psw i")
+let eyePsw = document.querySelector(".field.psw .eyePsw")
 console.log(eyePsw)
 eyePsw.addEventListener ("click", hola);
 function hola () {
-    if (eyePsw.className != "fas fa-eye-slash") {
-        eyePsw.className = "fas fa-eye-slash"
+    if (eyePsw.className != "fas fa-eye-slash eyePsw") {
+        eyePsw.className = "fas fa-eye-slash eyePsw"
         form.type = "text"
     }else {
-        eyePsw.className = "fas fa-eye"
+        eyePsw.className = "fas fa-eye eyePsw"
         form.type = "password"
     }
 }
-if (form) {
-    
-}
-let form2 = document.getElementById("password-confirm")
-let eyePsw2 = document.querySelector(".field.psw-confirm i")
-if (eyePsw2 != null) {
-    eyePsw2.addEventListener ("click", chau);
-function chau () {
-    if (eyePsw2.className != "fas fa-eye-slash") {
-        eyePsw2.className = "fas fa-eye-slash"
-        form2.type = "text"
-    }else {
-        eyePsw2.className = "fas fa-eye"
-        form2.type = "password"
+
+
+let buttonToDelete = document.getElementById("delete-button")
+buttonToDelete.onclick = () => {
+    let inputs = document.querySelectorAll(".field input")
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        input.value = ""
+        
     }
 }
-}
-
