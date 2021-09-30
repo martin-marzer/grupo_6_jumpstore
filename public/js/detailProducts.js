e1397114465e5b636689b68fdb42ccd674916d48
martin-marzer <martinvazquez1104@gmail.com>
2021-09-30 20:57:02 -0300

    detalle terminado

--- public/js/detailProducts.js
+++ public/js/detailProducts.js
@@ -1,25 +1,50 @@
 // Get the modal
-let modal = document.getElementById("myModal");
+let modal = document.getElementsByClassName("data");
 
 // Get the button that opens the modal
-let btnModal = document.getElementById("myBtn");
+let btnModal = document.getElementsByClassName("myBtn");
+
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close");
 
-// When the user clicks on the button, open the modal
-btnModal.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+console.log()
+for (let i = 0; i < modal.length; i++) {
+    if (modal[i].style.display = "block") {
+      if (i == 0) {
+        span[0].onclick = () => {
+          modal[i].style.display = "none";
+        }
+      }
+      else if (i == 1) {
+        span[1].onclick = function() {
+          modal[i].style.display = "none";
+        }
+      }
+      else if (i == 2) {
+        span[2].onclick = function() {
+          modal[i].style.display = "none";
+        }
+      }
+    
+    }
+  if ( modal[i].style.display = "none") {
+    if (i == 0) {
+      btnModal[0].onclick = function() {
+        modal[i].style.display = "block";
+      }
+    }
+    else if (i == 1) {
+      btnModal[1].onclick = function() {
+        modal[i].style.display = "block";
+      }
+    }
+    else if (i == 2) {
+      btnModal[2].onclick = function() {
+        modal[i].style.display = "block";
+      }
+    }
   }
+
 }
+
