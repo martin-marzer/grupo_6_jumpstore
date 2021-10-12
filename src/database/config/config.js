f017805e3ba08350d819a6a531737d141d94fd37
martin-marzer <martinvazquez1104@gmail.com>
2021-10-12 19:28:17 -0300

    heroku sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "",
-    "database": "jumpstore",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || "",
+    "database": process.env.DB_DATABASE || "jumpstore",
+    "host": process.env.DB_HOST || "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
