71769c86a8736180c12b08de99a908a1e682bbba
martin-marzer <martinvazquez1104@gmail.com>
2021-11-13 21:44:54 -0300

    backend validator users & products

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -13,34 +13,34 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const { validationResult } = require("express-validator")
 
 const controlador = {
-    administrator: (req,res) => {
+    administrator: (req, res) => {
         let promProducts = Product.findAll()
         let promUsers = User.findAll()
         Promise
-        .all(([promProducts, promUsers]))
-        .then(([products, users]) => {
-            res.render("administrator", {
-                articulos: products,
-                usuarios: users,
-                user: req.session.userLogged
-            });
-        })
-    },
-    allProducts: (req,res) => {
-        Product.findAll({
-                include: ["images"]
-        })
-        .then(articulos => {
-            res.render("administratorProducts", {
-                articulos: articulos,
-                toThousand: toThousand
+            .all(([promProducts, promUsers]))
+            .then(([products, users]) => {
+                res.render("administrator", {
+                    articulos: products,
+                    usuarios: users,
+                    user: req.session.userLogged
+                });
             })
-        });
     },
-    productCreate: (req,res) => {
+    allProducts: (req, res) => {
+        Product.findAll({
+            include: ["images"]
+        })
+            .then(articulos => {
+                res.render("administratorProducts", {
+                    articulos: articulos,
+                    toThousand: toThousand
+                })
+            });
+    },
+    productCreate: (req, res) => {
         res.render('productCreate');
     },
-    store: (req,res) => {
+    store: (req, res) => {
         const resultValidation = validationResult(req);
 
         if (resultValidation.errors.length > 0) {
@@ -61,164 +61,181 @@ const controlador = {
                 updatedAt: null,
                 quantity: req.body.stock
             })
-            .then (() => {
-                Product.count({
-                    col: 'Product.id'
-                })
-                .then(count => {
-                    images.forEach(image => {
-            
-                        ImagesProduct.create({
-                            url: image,
-                            productsID: count
+                .then(() => {
+                    Product.count({
+                        col: 'Product.id'
+                    })
+                        .then(count => {
+                            images.forEach(image => {
+
+                                ImagesProduct.create({
+                                    url: image,
+                                    productsID: count
+                                })
+
+                            })
+                            console.log(count);
+                            SizesProduct.create({
+                                sizeID: req.body.talle,
+                                productID: count
+                            })
+                                .then(() => {
+                                    res.redirect('/administrator/products')
+                                })
                         })
-                    
-                    })
-                    console.log(count);
-                    SizesProduct.create({
-                        sizeID: req.body.talle,
-                        productID: count
-                    })
-                    .then(()=> {
-                        res.redirect('/administrator/products')
-                    })   
+
                 })
-       
-            })    
-            .catch(error => res.send(error))
+                .catch(error => res.send(error))
         }
 
     },
-    productEdit: (req,res) => {
+    productEdit: (req, res) => {
         Product.findByPk(req.params.id, {
-            include: ['images', "brands", "sizes"]
+            include: ["brands", "sizes"]
         })
-        .then(product => {
-            res.render("productEdit", {
-                articulo: product,
-                toThousand: toThousand
-            });
-        })
-        .catch(error => res.send(error))
-    },
-    update: (req,res) => {
-        let productId = req.params.id;
-        Product
-        .update(
-            {
-                name: req.body.name,
-                price: req.body.precio,
-                description: req.body.descripcion,
-                brandID: req.body.marca,
-                discount: req.body.descuento,
-                createdAt: req.body.fechaEntrada,
-                quantity: req.body.stock
-            },
-            {
-                where: {id: productId}
+            .then(product => {
+                res.render("productEdit", {
+                    articulo: product,
+                    toThousand: toThousand
+                });
             })
-        .then(()=> {
-            res.redirect('/administrator/products')
-        })            
-        .catch(error => res.send(error))
-    }, 
-    delete: (req,res) => {
+            .catch(error => res.send(error))
+    },
+    update: (req, res) => {
+        const resultValidation = validationResult(req);
 
-        ImagesProduct.findAll ({
+        if (resultValidation.errors.length > 0) {
+            Product.findByPk(req.params.id, {
+                include: ["brands", "sizes"]
+            })
+                .then(product => {
+                    res.render("productEdit", {
+                        articulo: product,
+                        toThousand: toThousand,
+                        errors: resultValidation.mapped(),
+                        oldData: req.body
+                    });
+                })
+                .catch(error => res.send(error))
+        } else {
+            let productId = req.params.id;
+            Product
+                .update(
+                    {
+                        name: req.body.name,
+                        price: req.body.precio,
+                        description: req.body.descripcion,
+                        brandID: req.body.marca,
+                        discount: req.body.descuento,
+                        createdAt: req.body.fechaEntrada,
+                        quantity: req.body.stock
+                    },
+                    {
+                        where: { id: productId }
+                    })
+                .then(() => {
+                    res.redirect('/administrator/products')
+                })
+                .catch(error => res.send(error))
+        }
+    },
+    delete: (req, res) => {
+
+        ImagesProduct.findAll({
             where: {
                 productsID: req.params.id
             }
         })
-        .then((images)=> {
-            let arrIMG = [images[0].url, images[1].url, images[2].url]
-            arrIMG.forEach(image => {
-                fs.unlinkSync(ImagesFolderPath + image)
-            })
-        })
-        .then(() => {
-            ImagesProduct.destroy( {
-                where: {
-                    productsID: req.params.id
-                }, force: true
+            .then((images) => {
+                let arrIMG = [images[0].url, images[1].url, images[2].url]
+                arrIMG.forEach(image => {
+                    fs.unlinkSync(ImagesFolderPath + image)
+                })
             })
             .then(() => {
-                SizesProduct.destroy( {
+                ImagesProduct.destroy({
                     where: {
-                        productID: req.params.id,
-                        sizeID: req.body.talle
+                        productsID: req.params.id
                     }, force: true
                 })
-                .then(() => {
-                    Product.destroy( {
-                        where: {
-                            id: req.params.id
-                        }, force: true
-                    })
-                    // .then(()=>{
-                        
-                    //     Product.count({
-                    //         col: "Product.id"
-                    //     })
-                    //     .then(count => {
-                    //         for (let i = 1; i <= count; i++) {
-                    //             Product.findByPk(i)
-                    //             .then(producto => {
-                    //                 if (producto == undefined) {
-                    //                     Product.update(
-                    //                         { id: i},
-                    //                         { where: {id: i + 1} }
-                    //                     )
-                    //                 }
-                                    
-                    //             })
-                               
-                    //         }
-                    //     })
-    
-    
-                    // })
                     .then(() => {
-                        res.redirect('/administratorToolsProducts')
+                        SizesProduct.destroy({
+                            where: {
+                                productID: req.params.id,
+                                sizeID: req.body.talle
+                            }, force: true
+                        })
+                            .then(() => {
+                                Product.destroy({
+                                    where: {
+                                        id: req.params.id
+                                    }, force: true
+                                })
+                                    // .then(()=>{
+
+                                    //     Product.count({
+                                    //         col: "Product.id"
+                                    //     })
+                                    //     .then(count => {
+                                    //         for (let i = 1; i <= count; i++) {
+                                    //             Product.findByPk(i)
+                                    //             .then(producto => {
+                                    //                 if (producto == undefined) {
+                                    //                     Product.update(
+                                    //                         { id: i},
+                                    //                         { where: {id: i + 1} }
+                                    //                     )
+                                    //                 }
+
+                                    //             })
+
+                                    //         }
+                                    //     })
+
+
+                                    // })
+                                    .then(() => {
+                                        res.redirect('/administratorToolsProducts')
+                                    })
+                            })
+
+
                     })
-                })
-
-
             })
-        })
 
-        
-        .catch(error => res.send(error)) 
+
+            .catch(error => res.send(error))
     },
-    allUsers: (req,res) => {
+    allUsers: (req, res) => {
         User.findAll()
-        .then(usuarios => {
-            res.render("administratorUsers", {
-                usuarios: usuarios
-            })
-        });
-    },
-    userEdit: (req,res) => {
-        User.findByPk(req.params.id)
-        .then(user => {
-            res.render("userEdit", {
-                user: user,
+            .then(usuarios => {
+                res.render("administratorUsers", {
+                    usuarios: usuarios
+                })
             });
-        })
-        .catch(error => res.send(error))
     },
-    userUpdate: (req,res) => {
-        User
-        .update(
-            {
-                rol: req.body.rol
-            },
-            {
-                where: {id: req.params.id}
+    userEdit: (req, res) => {
+        User.findByPk(req.params.id)
+            .then(user => {
+                res.render("userEdit", {
+                    user: user,
+                });
             })
-        .then(()=> {
-            res.redirect('/administrator/users')
-        })            
-        .catch(error => res.send(error))
+            .catch(error => res.send(error))
+    },
+    userUpdate: (req, res) => {
+        User
+            .update(
+                {
+                    rol: req.body.rol
+                },
+                {
+                    where: { id: req.params.id }
+                })
+            .then(() => {
+                res.redirect('/administrator/users')
+            })
+            .catch(error => res.send(error))
     }
     // nose: (req, res) => {
     //     Product.count({
@@ -252,7 +269,7 @@ const controlador = {
     //             //     {
     //             //         where: {id: i}
     //             //     })
-            
+
     //             // console.log( eachwacho);
     //         }
 
@@ -260,7 +277,7 @@ const controlador = {
     //         //      return e + 1
     //         //  })
     //         // console.log(hola);
-            
+
     //         res.json(idk)
     //     })
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,94 +1,140 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const fs = require('fs');
 const path = require("path");
+const ImagesFolderPath = path.join(__dirname, '../../public/images/zapatillas/');
 const multer = require("multer");
 const { body } = require("express-validator")
 
 const multerStorage = multer.diskStorage({
-    destination:  (req, file, cb) => {
-      cb(null, path.join(__dirname, "../../public/images/zapatillas"));
-    },
-    filename: (req, file, cb) => {
-        const newFilename = "product" + '-' + Date.now() + path.extname(file.originalname);
-      cb(null, newFilename);
-    }
-  })
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../../public/images/zapatillas'));
+  },
+  filename: (req, file, cb) => {
+    const newFilename = "product" + '-' + Date.now() + path.extname(file.originalname);
+    cb(null, newFilename);
+  }
+})
 
-  const upload = multer({
-    storage: multerStorage,
-    fileFilter: function (req, file, callback) {
-        let ext = path.extname(file.originalname);
-        //aca se especifica los archivos q admite
-        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
-            return callback(new Error('solo imagenes pa'))
-        }
-        callback(null, true)
-    },
-    //aca se especifica los limites, se aceptan aprox 2MB each foto, and only 3 files sino no se sube
-    limits:{
-        fileSize: 1424 * 1424,
-        files: 3
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: function (req, file, callback) {
+    let ext = path.extname(file.originalname);
+    //aca se especifica los archivos q admite
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+      return callback(new Error('solo imagenes pa'))
     }
+    callback(null, true)
+  },
+  //aca se especifica los limites, se aceptan aprox 2MB each foto, and only 3 files sino no se sube
+  limits: {
+    fileSize: 1424 * 1424,
+    files: 3
+  }
 });
 
+function validUploadLength(req, res, next) {
+  if (req.files.length !== 3) {
+    // console.log(req.files);
+    let images;
+    if (req.files.length == 1) {
+      images = req.files[0].filename
+      fs.unlinkSync(ImagesFolderPath + images)
+    }
+    else if (req.files.length == 2) {
+      images = [req.files[0].filename, , req.files[1].filename]
+      images.forEach(image => {
+        fs.unlinkSync(ImagesFolderPath + image)
+      })
+    }
+    let e = new Error('Solo 3 imagenes')
+    res.locals.imgERROR = e.toString()
+  }
+  next()
+}
 
 
 const validations = [
   body("name")
-  .notEmpty().withMessage("Escribe un Nombre").bail()
-  .isLength({min:5, max:100}).withMessage("Longitud: 5 a 100 Caracteres"),
-
-  body("precio")
-  .notEmpty().withMessage("Escribe el precio").bail()
-  .isInt().withMessage("solo números"),
-
-  body("descripcion")
-  .notEmpty().withMessage("Escribe la descripcion").bail()
-  .isLength({min:1, max:200}).withMessage("Longitud: 1 a 200 Caracteres"),,
-
-  body("marca")
-  .notEmpty().withMessage("Elige la marca").bail()
-  .isInt().withMessage(),
-
-  body("descuento")
-  .notEmpty().withMessage("Escribe el descuento").bail()
-  .isInt().withMessage("solo números"),
+    .notEmpty().withMessage("Escribe un Nombre").bail()
+    .isLength({ min: 5, max: 100 }).withMessage("Longitud: 5 a 100 Caracteres"),
 
   body("fechaEntrada")
-  .notEmpty().withMessage("No puede estar vacío").bail()
-  .isDate().withMessage("Elige una fecha"),
+    .notEmpty().withMessage("No puede estar vacío").bail()
+    .isDate().withMessage("Elige una fecha"),
+
+  body("talle")
+    .notEmpty().withMessage("Elige el talle").bail()
+    .isInt().withMessage(),
+
+  body("marca")
+    .notEmpty().withMessage("Elige la marca").bail()
+    .isInt().withMessage(),
+
+  body("precio")
+    .notEmpty().withMessage("Escribe el precio").bail()
+    .matches(/^[0-9]*\.?[0-9]*$/).withMessage("solo números"),
+
+  body("descuento")
+    .notEmpty().withMessage("Escribe el descuento").bail()
+    .matches(/^[0-9]+$/).withMessage("solo números").bail()
+    .custom(value => {
+      if (value >= 0 && value <= 100) {
+        return true
+      } else {
+        return false
+      }
+
+    }).withMessage("de 0 a 100"),
 
   body("stock")
-  .notEmpty().withMessage("Escribe el descuento").bail()
-  .isInt().withMessage("solo números")
+    .notEmpty().withMessage("Escribe el descuento").bail()
+    .matches(/^[0-9]+$/).withMessage("solo números"),
+
+  // body("myFile")
+  // .custom(( value, { req }) => {
+  //   let file = req.file
+  //   let validExt = [".jpg", ".png", ".jpeg"]
+
+  //   if (!file) {
+  //     throw new Error("tenes que subir una imagenes")
+  //   }
+  //   return true
+  // }),
+
+  body("descripcion")
+    .notEmpty().withMessage("Escribe la descripcion").bail()
+    .isLength({ min: 1, max: 200 }).withMessage("Longitud: 1 a 200 Caracteres")
+
 ];
 
 // console.log(validations);
 // router.get("/holi", adminMiddleware, adminController.nose);
 
-/*** GET ADMINISTRATOR ***/  
+/*** GET ADMINISTRATOR ***/
 router.get("/", adminController.administrator);
 
-/*** GET TODOS LOS PRODUCTOS COMO ADMIN ***/  
+/*** GET TODOS LOS PRODUCTOS COMO ADMIN ***/
 router.get("/products", adminController.allProducts)
 
-/*** CREATE ONE PRDUCT ***/  
+/*** CREATE ONE PRDUCT ***/
 router.get("/products/create", adminController.productCreate)
-router.post('/products', upload.array("myFile"), adminController.store); 
+router.post('/products/create', upload.array("myFile", 3), validUploadLength, validations, adminController.store);
 
-/*** EDIT ONE PRDUCT ***/  
+/*** EDIT ONE PRDUCT ***/
 router.get("/products/edit/:id", adminController.productEdit)
-router.put('/products/edit/:id', adminController.update);
+router.put('/products/edit/:id', validations, adminController.update);
 
 // /*** DELETE ONE PRODUCT***/ 
-router.delete('/products/delete/:id', adminController.delete); 
+router.delete('/products/delete/:id', adminController.delete);
 
-/*** GET ALL USERS AS ADMIN ***/  
+/*** GET ALL USERS AS ADMIN ***/
 router.get("/users", adminController.allUsers)
 
-/*** EDIT ONE USER ***/  
+/*** EDIT ONE USER ***/
 router.get("/users/edit/:id", adminController.userEdit)
 router.put('/users/edit/:id', adminController.userUpdate);
+
 
 module.exports = router;
