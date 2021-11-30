7de13e36e07b915622746357204665fc63439085
martin-marzer <martinvazquez1104@gmail.com>
2021-11-30 15:52:10 -0300

    favoritos dinamicos terminado

--- public/js/favs.js
+++ public/js/favs.js
@@ -1,184 +1,184 @@
 window.addEventListener("load", () => {
+  let getUserID = document.querySelector(".dropdown-content #user").dataset.test;
 
+    let agregarFav = (id) => {
 
-  let agregarFav = (id) => {
+        let datos = localStorage.getItem(`id${getUserID}`);
+        if (datos !== null) {
+            let arr = datos.split(',');
+            arr.push(id);
+            localStorage.setItem(`id${getUserID}`, arr);
 
-    let datos = sessionStorage.getItem('id');
-    if (datos !== null) {
-      let arr = datos.split(',');
-      arr.push(id);
-      sessionStorage.setItem('id', arr);
-
-    } else {
-      sessionStorage.setItem('id', id);
-      window.location = window.location
-    }
-
-  }
-
-
-  let quitarFav = (id) => {
-
-    let datos = sessionStorage.getItem('id');
-    if (datos !== null) {
-      let arr = datos.split(',');
-
-      arr = arr.filter(i => {
-        return i != id
-      })
-      sessionStorage.setItem('id', arr);
-
-      if (arr.length == 0) {
-        sessionStorage.removeItem('id');
-      }
-      window.location = window.location
+        } else {
+            localStorage.setItem(`id${getUserID}`, id);
+            window.location = window.location
+        }
 
     }
-    else {
-      sessionStorage.removeItem('id');
+
+
+    let quitarFav = (id) => {
+
+        let datos = localStorage.getItem(`id${getUserID}`);
+        if (datos !== null) {
+            let arr = datos.split(',');
+
+            arr = arr.filter(i => {
+                return i != id
+            })
+            localStorage.setItem(`id${getUserID}`, arr);
+
+            if (arr.length == 0) {
+                localStorage.removeItem(`id${getUserID}`);
+            }
+            window.location = window.location
+
+        } else {
+            localStorage.removeItem(`id${getUserID}`);
+        }
+
     }
 
-  }
+    const app = document.querySelector(".products-container");
 
-  const app = document.querySelector(".products-container");
+    let str = localStorage.getItem(`id${getUserID}`);
+    console.log(str);
 
-  let str = sessionStorage.getItem('id');
-  console.log(str);
+    const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-  const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    if (str != null) {
+        let arr = str.split(',');
+        let favoritas = [...new Set(arr)]
 
-  if (str != null) {
+        favoritas.forEach((id) => {
+            fetch(`api/products/${id}`)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(products => {
+                    let product = products.data;
 
-    let arr = str.split(',');
-    let favoritas = [...new Set(arr)]
+                    let count = document.querySelector(".results-container p")
+                    count.innerHTML =
+                        `<strong>${favoritas.length}</strong> Productos Favoritos`
 
-    favoritas.forEach((id) => {
-      fetch(`api/products/${id}`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(products => {
-          let product = products.data;
-
-          let count = document.querySelector(".results-container p")
-          count.innerHTML = `<strong>${favoritas.length}</strong> Productos Favoritos`
-
-          let div = document.createElement("div")
-          div.classList.add("product-container")
+                    let div = document.createElement("div")
+                    div.classList.add("product-container")
 
 
-          if (product.discount == 0) {
-            div.innerHTML = `
-            <a href="/sneakers/detail/${product.id}" class="link-detail">
-                <div class="product">
-                    
-                    <div class="info-product">
-                        <div class="img-container">
-                            <img src="${product.images[0].url}" alt="producto" class="img-product">
-                        </div>
-                        <div class="text">
-                            <div class="sneaker-name">
-                                <p>${product.name}</p>
-                            </div>
-                            <div class="price">
-
-                                <p> $ ${toThousand(product.price)}</p>
-
-                            </div>                                             
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <div class="container-heart">
-                <i class="far fa-heart"></i>
-            </div>            
-            `;
-          } else {
-            div.innerHTML = `
-            <a href="/sneakers/detail/${product.id}" class="link-detail">
-                <div class="product">
-                    
-                    <div class="sale">
-                      <div class="discount">
-                          ${product.discount}% OFF
-                      </div>
+                    if (product.discount == 0) {
+                        div.innerHTML = `
+  <a href="/sneakers/detail/${product.id}" class="link-detail">
+      <div class="product">
+          
+          <div class="info-product">
+              <div class="img-container">
+                  <img src="${product.images[0].url}" alt="producto" class="img-product">
+              </div>
+              <div class="text">
+                  <div class="sneaker-name">
+                      <p>${product.name}</p>
                   </div>
+                  <div class="price">
 
-                    <div class="info-product">
-                        <div class="img-container">
-                            <img src="${product.images[0].url}" alt="producto" class="img-product">
-                        </div>
-                        <div class="text">
-                            <div class="sneaker-name">
-                                <p>${product.name}</p>
-                            </div>
-                            <div class="price">
+                      <p> $ ${toThousand(product.price)}</p>
 
-                              <p>$${toThousand(product.price - (product.price * product.discount / 100))} </p>
-                              <p class="oldPrice">$${toThousand(product.price)}</p>
+                  </div>                                             
+              </div>
+          </div>
+      </div>
+  </a>
+  <div class="container-heart">
+      <i class="far fa-heart"></i>
+  </div>            
+  `;
+                    } else {
+                        div.innerHTML = `
+  <a href="/sneakers/detail/${product.id}" class="link-detail">
+      <div class="product">
+          
+          <div class="sale">
+            <div class="discount">
+                ${product.discount}% OFF
+            </div>
+        </div>
 
-                            </div>                                             
-                        </div>
-                    </div>
-                </div>
-            </a>
-            <div class="container-heart">
-                <i class="far fa-heart"></i>
-            </div>            
-            `;
-          }
+          <div class="info-product">
+              <div class="img-container">
+                  <img src="${product.images[0].url}" alt="producto" class="img-product">
+              </div>
+              <div class="text">
+                  <div class="sneaker-name">
+                      <p>${product.name}</p>
+                  </div>
+                  <div class="price">
 
+                    <p>$${toThousand(product.price - (product.price * product.discount / 100))} </p>
+                    <p class="oldPrice">$${toThousand(product.price)}</p>
 
-          app.appendChild(div)    
-          let star = div.children[1].children[0]
-          console.log(star);
-
-
-
-          let starSelect = (id) => {
-            let datos = sessionStorage.getItem('id');
-            if (datos != null) {
-              let arr = datos.split(',');
-              if (arr.includes(id.toString())) {
-                star.classList.add("fas")
-                star.classList.remove("far")
-              }
-            }
-          }
-
-          starSelect(product.id)
-
-          star.addEventListener("click", (e) => {
-
-            e.preventDefault()
-            if (star.classList.value.indexOf("far") != -1) {
-              star.classList.add("fas")
-              star.classList.remove("far")
-              star.onclick = agregarFav(product.id)
+                  </div>                                             
+              </div>
+          </div>
+      </div>
+  </a>
+  <div class="container-heart">
+      <i class="far fa-heart"></i>
+  </div>            
+  `;
+                    }
 
 
-              console.log(sessionStorage.getItem("id"));
+                    app.appendChild(div)
+                    let star = div.children[1].children[0]
+                    console.log(star);
 
-            } else {
-              star.classList.remove("fas")
-              star.classList.add("far")
-              star.onclick = quitarFav(product.id)
 
-              console.log(sessionStorage.getItem("id"));
-            }
 
-          })
+                    let starSelect = (id) => {
+                        let datos = localStorage.getItem(`id${getUserID}`);
+                        if (datos != null) {
+                            let arr = datos.split(',');
+                            if (arr.includes(id.toString())) {
+                                star.classList.add("fas")
+                                star.classList.remove("far")
+                            }
+                        }
+                    }
 
-        })
-        .catch(function (error) {
-          console.error(error);
+                    starSelect(product.id)
+
+                    star.addEventListener("click", (e) => {
+
+                        e.preventDefault()
+                        if (star.classList.value.indexOf("far") != -1) {
+                            star.classList.add("fas")
+                            star.classList.remove("far")
+                            star.onclick = agregarFav(product.id)
+
+
+                            console.log(localStorage.getItem("id"));
+
+                        } else {
+                            star.classList.remove("fas")
+                            star.classList.add("far")
+                            star.onclick = quitarFav(product.id)
+
+                            console.log(localStorage.getItem("id"));
+                        }
+
+                    })
+
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+
         });
 
-    });
-
-  } else {
-    app.innerHTML = `<p> No tienes absolutamente nada en favoritos, ¡ve a la <a href="/sneakers/OrderByReleaseDateASC"> tienda! </a> </p>`;
-  }
+    } else {
+        app.innerHTML =
+            `<p> No tienes absolutamente nada en favoritos, ¡ve a la <a href="/sneakers/OrderByReleaseDateASC"> tienda! </a> </p>`;
+    }
 
 
 })
--- public/js/listProductsFav.js
+++ public/js/listProductsFav.js
@@ -1,82 +1,82 @@
 window.addEventListener("load", () => {
+    let getUserID = document.querySelector(".dropdown-content #user").dataset.test;
 
     let agregarFav = (id) => {
-    
-      let datos = sessionStorage.getItem('id');
-      if (datos !== null) {
-        let arr = datos.split(',');
-        arr.push(id);
-        sessionStorage.setItem('id', arr);
-  
-      } else {
-        sessionStorage.setItem('id', id);
-      }
-  
-    }
-  
-  
-    let quitarFav = (id) => {
-    
-      let datos = sessionStorage.getItem('id');
-      if (datos !== null) {
-        let arr = datos.split(',');
-  
-        arr = arr.filter(i => {
-          return i != id
-        })
-        sessionStorage.setItem('id', arr);
-  
-        if (arr.length == 0) {
-          sessionStorage.removeItem('id');
+
+        let datos = localStorage.getItem(`id${getUserID}`);
+        if (datos !== null) {
+            let arr = datos.split(',');
+            arr.push(id);
+            localStorage.setItem(`id${getUserID}`, arr);
+
+        } else {
+            localStorage.setItem(`id${getUserID}`, id);
         }
-        
-      } 
-      else {
-        sessionStorage.removeItem('id');
-      }
-  
+
     }
-  
+
+
+    let quitarFav = (id) => {
+
+        let datos = localStorage.getItem(`id${getUserID}`);
+        if (datos !== null) {
+            let arr = datos.split(',');
+
+            arr = arr.filter(i => {
+                return i != id
+            })
+            localStorage.setItem(`id${getUserID}`, arr);
+
+            if (arr.length == 0) {
+                localStorage.removeItem(`id${getUserID}`);
+            }
+
+        } else {
+            localStorage.removeItem(`id${getUserID}`);
+        }
+
+    }
+
     let allProducts = document.querySelectorAll(".product-container")
     let productsArr = [...allProducts]
     productsArr.forEach(product => {
-      const star =  product.children[1].children[0];
-      const linkID = product.children[0].href.split("/")[5]
-      // console.log(star);
-      
-      let starSelect = (id) => {
-        let datos = sessionStorage.getItem('id');
-        if (datos != null) {
-          let arr = datos.split(',');
-          if (arr.includes(id.toString() ) ) {
-            star.classList.add("fas")
-            star.classList.remove("far")
-          }
+        const star = product.children[1].children[0];
+        const linkID = product.children[0].href.split("/")[5]
+        // console.log(star);
+
+        let starSelect = (id) => {
+            let datos = localStorage.getItem(`id${getUserID}`);
+            if (datos != null) {
+                let arr = datos.split(',');
+                if (arr.includes(id.toString())) {
+                    star.classList.add("fas")
+                    star.classList.remove("far")
+                }
+            }
         }
-      }
-    
-      starSelect( linkID )
-    
-      star.addEventListener("click", (e) => { 
-        
-        e.preventDefault()
-        if (star.classList.value.indexOf("far") != -1 ) {
-          star.classList.add("fas")
-          star.classList.remove("far")
-          star.onclick = agregarFav(linkID)
-    
-          
-          console.log(sessionStorage.getItem("id"));
-          
-        } else {
-          star.classList.remove("fas")
-          star.classList.add("far")
-          star.onclick = quitarFav(linkID)
-    
-          console.log(sessionStorage.getItem("id"));
-        }
-    
-      })
+
+        starSelect(linkID)
+
+        star.addEventListener("click", (e) => {
+
+            e.preventDefault()
+            if (star.classList.value.indexOf("far") != -1) {
+                star.classList.add("fas")
+                star.classList.remove("far")
+                star.onclick = agregarFav(linkID)
+
+
+                console.log(localStorage.getItem(`id${getUserID}`));
+
+            } else {
+                star.classList.remove("fas")
+                star.classList.add("far")
+                star.onclick = quitarFav(linkID)
+
+                console.log(localStorage.getItem(`id${getUserID}`));
+            }
+
+        })
     })
-    
+
 })
