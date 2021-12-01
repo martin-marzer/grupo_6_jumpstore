304ccb678067f70b1c6aa5b7198ab47d4482cc19
martin-marzer <martinvazquez1104@gmail.com>
2021-12-01 04:09:09 -0300

    cors installed

--- src/routes/api/products.js
+++ src/routes/api/products.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const productsAPIController = require("../../controllers/api/productsAPIController");
+const cors = require('cors')
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 //primero tiro la data para mostrar (carga el orden de los productos)
-router.get("/api/products", productsAPIController.list);
+router.get("/api/products", cors(corsOptions), productsAPIController.list);
 
 router.get("/api/products/:id", productsAPIController.detail);
 
